7acbfee8ff47ab8150162bf8c9b2981220f5705a
Ronni Chidekel <ronnidc@gmail.com>
2016-11-02 21:02:35 +0100

    content changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pagetitle = 'My Angular2 App';
-  articletitle = 'Let\'s work with Angular';
+  articletitle = 'Let\'s play with Angular';
   articlecontent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   articleauthor = 'Ronni DC';
 }
